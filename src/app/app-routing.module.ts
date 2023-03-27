ecaf5fb96da94662c0bbdf53d8a4ec3d856fd924
Morgane-Compa <morgane.compafloury@gmail.com>
2023-03-27 16:51:56 +0200

    html et css des deux components ainsi qu'ajout des images dans le asset et ajout de CSS global

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreationTasksComponent } from './pages/creation-tasks/creation-tasks.component';
+import { HistoryTasksComponent } from './pages/history-tasks/history-tasks.component';
+import { ToDoTaskComponent } from './pages/to-do-task/to-do-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: ToDoTaskComponent,
+  },
+  {
+    path: "création",
+    component: CreationTasksComponent
+  },
+  {
+    path: "hisotry",
+    component: HistoryTasksComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/creation-tasks/creation-tasks.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creation-tasks',
+  templateUrl: './creation-tasks.component.html',
+  styleUrls: ['./creation-tasks.component.css']
+})
+export class CreationTasksComponent {
+
+}

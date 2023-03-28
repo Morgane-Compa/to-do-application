5e605ad9b852493fe7b994998bab4f787f16a6c6
Morgane-Compa <morgane.compafloury@gmail.com>
2023-03-28 09:15:40 +0200

    ajout d'un type dans un fichier mock

---
+++ src/app/mocks/to-do-type.mock.ts
@@ -0,0 +1,10 @@
+type CategoryType = "shopping" | "health" | "work" | "bills" | "cleaning" | "other";
+
+interface ITodo {
+id: number;
+content: string;
+category: CategoryType;
+isUrgent: boolean;
+doneDate: Date;
+isDone: boolean
+}
